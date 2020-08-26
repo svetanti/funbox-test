20426d33c5b90dbbf34b53ac7b6ea3c18773cdf5
Svetlana Antipova <die-chimaere@yandex.ru>
2020-08-26 18:35:58 +0300

    Updates

---
+++ precache-manifest.1dc93c57ade1a07454527f94cabc99ea.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ed2dc5f56a68bf5f18d85bae76ec089",
+    "url": "./index.html"
+  },
+  {
+    "revision": "239c5e61bf65a7f1cda8",
+    "url": "./static/css/main.0694dc99.chunk.css"
+  },
+  {
+    "revision": "4c5b7e229e6ccc015e00",
+    "url": "./static/js/2.3d6997c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.3d6997c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "239c5e61bf65a7f1cda8",
+    "url": "./static/js/main.2fea57a6.chunk.js"
+  },
+  {
+    "revision": "ec6ea16117930cc2ce48",
+    "url": "./static/js/runtime-main.ddffc4f5.js"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "./static/media/Exo2.0-Thin.44bde874.otf"
+  },
+  {
+    "revision": "c4c199b57646554d09c60cc868f5a566",
+    "url": "./static/media/cat.c4c199b5.png"
+  },
+  {
+    "revision": "8e412cf5e5954e6134e979e7f9cb9e17",
+    "url": "./static/media/pattern.8e412cf5.png"
+  }
+]);
